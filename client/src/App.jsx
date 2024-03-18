ac2d09da38f7b9592317241a3f5d1eea5c389dd4
Scorti-Paul <scortip@gmail.com>
2024-03-18 13:32:03 +0000

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./Components/Header";
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+    </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/pages/SignUp/index.jsx
@@ -0,0 +1,8 @@
+
+const SignUp = () => {
+  return (
+    <div className="text-indigo-500">SignUp</div>
+  )
+}
+
+export default SignUp
